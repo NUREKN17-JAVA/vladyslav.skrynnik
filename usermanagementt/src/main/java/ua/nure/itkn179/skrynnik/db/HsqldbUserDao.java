2c2e3de84017dd703eab6269571d6c3541e3ec54
Vladyslav Skrynnik <vladyslav.skrynnik@nure.ua>
2019-11-05 22:21:35 +0200

    Second step

---
+++ usermanagementt/src/main/java/ua/nure/itkn179/skrynnik/db/HsqldbUserDao.java
@@ -0,0 +1,40 @@
+package ua.nure.itkn179.skrynnik.db;
+
+import java.util.Collection;
+import ua.nure.itkn179.skrynnik.usermanagement.User;
+import ua.nure.itkn179.skrynnik.usermanagement.db.Dao;
+import ua.nure.itkn179.skrynnik.usermanagement.db.DatabaseException;
+
+public class HsqldbUserDao implements Dao<User> {
+
+	@Override
+	public User create(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(User entity) throws DatabaseException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User find(long id) throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<User> findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ usermanagementt/src/test/java/ua/nure/itkn179/skrynnik/db/HsqldbUserDaoTest.java
@@ -0,0 +1,7 @@
+package ua.nure.itkn179.skrynnik.db;
+
+import junit.framework.TestCase;
+
+public class HsqldbUserDaoTest extends TestCase {
+
+}
