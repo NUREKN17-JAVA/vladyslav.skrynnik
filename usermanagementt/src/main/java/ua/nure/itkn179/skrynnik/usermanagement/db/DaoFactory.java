32ddda0685bb53f2b33063cd1c2966b4797d5793
Vladyslav Skrynnik <vladyslav.skrynnik@nure.ua>
2019-11-06 00:15:24 +0200

    setting.properities and DaoFactory

---
+++ usermanagementt/src/main/java/ua/nure/itkn179/skrynnik/usermanagement/db/DaoFactory.java
@@ -0,0 +1,18 @@
+package ua.nure.itkn179.skrynnik.usermanagement.db;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class DaoFactory {
+	private final Properties properties;
+
+	public DaoFactory() {
+		properties = new Properties();
+		try {
+			properties.load(getClass().getClassLoader().getResourceAsStream("settings.properties"));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
